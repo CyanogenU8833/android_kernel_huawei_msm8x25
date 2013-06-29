1cb97fd5592e602e020419e95f43c4822a1ebefb
josalaito <josalaito@hotmail.es>
2013-06-29 19:43:56 +0200

    Fixed build

---
+++ include/linux/netfilter_ipv6/ip6t_HL.h
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ip6t_HL.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2013年01月20日 22时55分31秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Qiwu (ivan), huangqiwu@shendu.com
+ *        Company:  shendu
+ *
+ * =====================================================================================
+ */
+/*  Hop Limit modification module for ip6tables
+ *  * Maciej Soltysiak <solt@dns.toxicfilms.tv>
+ *  * Based on HW's TTL module */
+
+#ifndef _IP6T_HL_H
+#define _IP6T_HL_H
+
+#include <linux/types.h>
+
+enum {
+    IP6T_HL_SET = 0,
+    IP6T_HL_INC,
+    IP6T_HL_DEC
+};
+
+#define IP6T_HL_MAXMODE IP6T_HL_DEC
+
+struct ip6t_HL_info {
+    __u8    mode;
+    __u8    hop_limit;
+};
+
+
+#endif
